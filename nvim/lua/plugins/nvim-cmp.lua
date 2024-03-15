dbf8b00e27b18caddb7cef5369640ab499d031c9
Andrea Longo <andrea.longo@smartpricing.it>
2024-03-15 17:29:22 +0100

    feat(cmp): change autocomplete keystroke

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -21,7 +21,7 @@ return {
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-u>"] = cmp.mapping.scroll_docs(-4),
         ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ["<A-Space>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-c>"] = cmp.mapping.confirm({ select = true }),
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp_signature_help" },
