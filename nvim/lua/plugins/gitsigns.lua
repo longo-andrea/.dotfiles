bfd80687b7c5f8950f051a8236f792600b3994e2
Andrea Longo <andrea.longo@smartpricing.it>
2024-03-19 08:59:19 +0100

    feat(git-signs): add gitsigns

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,19 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    on_attach = function(buffer)
+      local gitsigns = package.loaded.gitsigns
+
+      local function map(mode, key, command, description)
+        vim.keymap.set(mode, key, command, { buffer = buffer, desc = description })
+      end
+
+      map("n", "<leader>tb", function()
+        gitsigns.toggle_current_line_blame()
+      end, { desc = "toggle line blame" })
+      map("n", "<leader>bl", function()
+        gitsigns.blame_line({ full = true })
+      end, "Blame Line")
+    end,
+  },
+}
