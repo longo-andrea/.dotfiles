fe0ec805699a6e4e0ba80d8df8d5a8dfda54d4ce
Andrea Longo <andrea.longo@smartpricing.it>
2024-03-25 15:22:34 +0100

    feat(vue-language-tools): bump major version

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -8,6 +8,9 @@ return {
   config = function(_, opts)
     local lspconfig = require("lspconfig")
     local mason_lspconfig = require("mason-lspconfig")
+    local mason_registry = require("mason-registry")
+    local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+      .. "/node_modules/@vue/language-server"
     local mason_lspconfig_handlers = {
       -- The first entry (without a key) will be the default handler
       -- and will be called for each installed server that doesn't have
@@ -29,7 +32,15 @@ return {
       end,
       ["tsserver"] = function()
         lspconfig["tsserver"].setup({
+          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
           init_options = {
+            plugins = {
+              {
+                name = "@vue/typescript-plugin",
+                location = vue_language_server_path,
+                languages = { "vue" },
+              },
+            },
             preferences = {
               -- other preferences...
               importModuleSpecifierPreference = "relative",
@@ -39,6 +50,7 @@ return {
         })
       end,
     }
+    lspconfig.volar.setup({})
     mason_lspconfig.setup(opts)
     mason_lspconfig.setup_handlers(mason_lspconfig_handlers)
   end,
