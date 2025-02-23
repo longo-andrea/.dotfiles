3ff020038afc110dd49aa5bcabd5e22e2aaed2dc
longo-andrea <longoandrea.dev@gmail.com>
2025-02-23 16:41:48 +0100

    feat(nvim): configure dashboard

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -20,6 +20,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- Reopen buffer at the latest cursor position.
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local last_pos = vim.fn.line("'\"")
+    if last_pos > 1 and last_pos <= vim.fn.line("$") then
+      vim.api.nvim_win_set_cursor(0, { last_pos, 0 })
+    end
+  end,
+})
+
 -- Setup LSP keymaps.
 --  See `:help vim.lsp.buf.*`
 vim.api.nvim_create_autocmd("LspAttach", {
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,6 @@
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 
-vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
