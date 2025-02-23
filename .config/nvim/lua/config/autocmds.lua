08810230b3ebb2df77f6691e77696d160863c06b
longo-andrea <longoandrea.dev@gmail.com>
2025-02-23 15:26:19 +0100

    feat(nvim): configure lsp keymaps

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -14,8 +14,38 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 -- Highlight yanked text region
 vim.api.nvim_create_autocmd("TextYankPost", {
 
-    group = create_augroup("highlight_yank"),
+  group = create_augroup("highlight_yank"),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- Setup LSP keymaps.
+--  See `:help vim.lsp.buf.*`
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "Format current buffer on save",
+  group = vim.api.nvim_create_augroup("lsp-keymaps", {}),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if not client then
+      return
+    end
+
+    local map = function(mode, lhs, rhs, opts)
+      opts = opts or {}
+      opts.buffer = args.buf
+
+      vim.keymap.set(mode, lhs, rhs, opts)
+    end
+
+    -- Default LSP capabilities' keymaps.
+    map("n", "gd", vim.lsp.buf.definition)
+    map("n", "gr", vim.lsp.buf.references)
+    map("n", "K", function()
+      vim.lsp.buf.hover({ border = "single" })
+    end)
+    map("n", "<leader>rn", vim.lsp.buf.rename)
+    map("n", "<leader>ca", vim.lsp.buf.code_action)
   end,
 })
